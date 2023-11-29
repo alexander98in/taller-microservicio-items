b622e07855473d7ff9bc0c46bedf44f1ede8261e
alexander98in <alexander98in@gmail.com>
2023-11-28 19:01:11 -0500

    Mi primer commit

---
+++ src/main/java/com/isbcs/app/items/IsbcsTallerServicioItemsApplication.java
@@ -0,0 +1,13 @@
+package com.isbcs.app.items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsbcsTallerServicioItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsbcsTallerServicioItemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isbcs/app/items/IsbcsTallerServicioItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isbcs.app.items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsbcsTallerServicioItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
